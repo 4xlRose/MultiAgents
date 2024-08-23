278179f42ad820a82c9438f3471e36e44107f61d
Axel Ariel Grande Ruiz <44534301+4xlRose@users.noreply.github.com>
2024-08-22 22:19:36 -0600

    Primer commit con los documentos
    Archivos para el modelo de los agentes

---
+++ Kit-Robots/agent.py
@@ -0,0 +1,164 @@
+from mesa.agent import Agent
+import numpy as np
+
+
+class Bot(Agent):
+
+    MAX_NUM_TRAINING_STEPS = 1000
+    NUM_OF_ACTIONS = 4
+
+    # Define the movements (0: down, 1: right, 2: up, 3: left)
+    MOVEMENTS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+    def __init__(self, unique_id, model, q_file=None):
+        super().__init__(unique_id, model)
+        self.q_values = None
+        self.done = False
+        self.state = None
+        self.next_state = None
+        self.action = None
+        self.next_pos = None
+        self.total_return = 0
+        self.training_step = 0
+        self.movements = 0
+
+        self.epsilon = 0.1
+        self.alpha = 0.1
+        self.gamma = 0.9
+
+        self.num_states = self.model.grid.width * self.model.grid.height
+
+        if q_file is None:
+            self.reset_q_values()
+        else:
+            self.load_q_values(q_file)
+
+    def reset_q_values(self):
+        self.q_values = {(state, action): np.random.uniform(0, .01)
+                         for state in range(self.num_states)
+                         for action in range(self.NUM_OF_ACTIONS)}
+
+    def step(self) -> None:
+        if self.state is None:
+            self.state = self.model.states[self.pos]
+
+        # Agent chooses an action from the policy
+        self.action = self.eps_greedy_policy(self.state)
+
+        # Get the next position
+        self.next_pos = self.perform(self.pos, self.action)
+        self.next_state = self.model.states[self.next_pos]
+
+    def advance(self) -> None:
+        # Check if the agent can move to the next position
+        if self.model.grid.is_cell_empty(self.next_pos) or self.next_state in self.model.goal_states:
+            if self.next_state in self.model.goal_states:
+                # Remove the goal agent from the grid
+                self.model.grid.remove_agent(self.model.grid.get_cell_list_contents(self.next_pos)[0])
+                self.done = True
+
+            # Move the agent to the next position and update everything
+            self.model.grid.move_agent(self, self.next_pos)
+            self.movements += 1
+
+            # Update the state
+            self.state = self.next_state
+
+            # Get the reward
+            reward = self.model.rewards[self.next_state]
+
+        else:
+            # If the agent cannot move to the next position, the reward is -2
+            reward = -2
+
+        # Update the q-values
+        self._update_q_values(self.state, self.action, reward, self.next_state)
+
+        # Update the total return
+        self.total_return += reward
+
+    def save_q_values(self):
+        np.save(f"./q_values{self.unique_id}.npy", self.q_values)
+
+    def train(self):
+        #pass
+        G_history = []
+        G_prom_hist = []
+        
+        for episodes in range(1000):
+            done = False
+            G = 0
+            episode_step = 1
+            
+            action = None
+            pos = self.pos
+            state = self.state
+            
+            reward = 1
+            
+            while not done:
+                next_state = None
+                episode_step += 1
+                
+                action = self.random_policy()
+                
+                pos = self.perform(pos, action)
+                #next_state, reward, done, _ = env.step(action)
+
+                Q_list_next_state = [self.q_values[(next_state, act)] for act in range(self.NUM_OF_ACTIONS)]  #env.action_space.n
+                #update rule
+                self.q_values[(state, action)] += self.alpha * (reward + self.gamma * np.max(Q_list_next_state) - self.q_values[(state, action)])
+                
+                G += reward
+                state = next_state  
+            
+            G_history.append(G)
+            G_prom_hist.append(G/episode_step)     
+            
+            if episodes % (1000 // 10) == 0:
+                print(f"Episodio {episodes}, Recompensa total: {G}",
+                    f"Recompensa promedio: {np.array(G_history).mean()} ")
+
+        return self.q_values
+    
+
+    def load_q_values(self, q_file):
+        try:
+            self.q_values = np.load(q_file, allow_pickle=True).item()
+            print(f"Q-values from {q_file} have been loaded.")
+        except FileNotFoundError:
+            self.reset_q_values()
+            print("File not found. Q-values have been reset.")
+
+    def perform(self, pos, action) -> tuple:
+        x = pos[0] + self.MOVEMENTS[action][0]
+        y = pos[1] + self.MOVEMENTS[action][1]
+        next_pos = (x, y)
+        return next_pos
+
+    def random_policy(self):
+        return np.random.randint(self.NUM_OF_ACTIONS)
+
+    def eps_greedy_policy(self, state):
+        if self.training_step < self.MAX_NUM_TRAINING_STEPS or np.random.rand() < self.epsilon:
+            self.training_step += 1
+            return self.random_policy()
+        else:
+            q_values = [self.q_values[state, action] for action in range(self.NUM_OF_ACTIONS)]
+            return np.argmax(q_values)
+
+    def _update_q_values(self, state, action, reward, next_state):
+        q_values = [self.q_values[next_state, action] for action in range(self.NUM_OF_ACTIONS)]
+        max_q_value = np.max(q_values)
+        self.q_values[state, action] += self.alpha * (
+                reward + self.gamma * max_q_value - self.q_values[state, action])
+
+
+class Box(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+
+class Goal(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
